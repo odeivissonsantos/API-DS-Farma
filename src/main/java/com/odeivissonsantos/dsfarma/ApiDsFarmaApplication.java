502f5e9580dea0026404f235344d48490199fc0a
odeivissonsantos <deivissonsantos@hotmail.com>
2021-09-05 09:49:07 -0300

    Inicialização do projeto e seus pacotes

---
+++ src/main/java/com/odeivissonsantos/dsfarma/ApiDsFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.dsfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDsFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDsFarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odeivissonsantos/dsfarma/ApiDsFarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.dsfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDsFarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
